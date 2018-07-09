52e11c2a589fcf910f9b3933fed1b262a9454985
MinhTienPro <tranminhtien1509@gmail.com>
2018-07-09 11:39:45 +0700

    Project update the Seven

--- app/src/main/java/com/tvt/projectcuoikhoa/utils/Const.java
+++ app/src/main/java/com/tvt/projectcuoikhoa/utils/Const.java
@@ -2,7 +2,7 @@ package com.tvt.projectcuoikhoa.utils;
 
 public class Const {
 
-    public static  final String BASE_URL="http://192.168.1.73/thegioididong/";
+    public static  final String BASE_URL="https://trantien1509.000webhostapp.com/thegioididong/";
     public static final String TAG="tien.mtp";
     public static final String URL_DEFAULT_PROFILE_GOOGLE ="https://1.bp.blogspot.com/-j7x6gBhBI8M/WvFpfoxwj6I/AAAAAAAABl8/UbF3XqFJALQRYFVvJqBixJ8rMQQtALgEACEwYBhgL/s1600/fb-nene-4.jpg" ;
 }
